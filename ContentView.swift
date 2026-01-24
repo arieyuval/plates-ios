1f8ee5a98d8807ae38dde59cc924025a43f9033a
Yuval <arieyuval33@gmail.com>
2026-01-23 17:09:28 -0800

    first

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plates
+//
+//  Created by Yuval Arie on 1/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatesApp.swift
+//  Plates
+//
+//  Created by Yuval Arie on 1/23/26.
+//
+
+import SwiftUI
+
+@main
+struct PlatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
